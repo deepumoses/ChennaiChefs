529c94964d24c586cdad58595d3bc7260eb9d4d6
deepak <sonicdeepak@gmail.com>
2020-01-11 16:30:05 +0530

    First commit for Restaurant app

---
+++ precache-manifest.ad5a601cc07a39608ac3e4a18c7aa732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91342273887f988233530f39f5f61684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a38e11143f3c7c8341",
+    "url": "/static/css/main.15bec2f7.chunk.css"
+  },
+  {
+    "revision": "362ee3e80a8995ddd189",
+    "url": "/static/js/2.46f61ee3.chunk.js"
+  },
+  {
+    "revision": "d3a38e11143f3c7c8341",
+    "url": "/static/js/main.23c3e72e.chunk.js"
+  },
+  {
+    "revision": "def789dda24a4bf87d13",
+    "url": "/static/js/runtime-main.5fed59b3.js"
+  }
+]);
